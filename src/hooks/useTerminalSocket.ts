43a6b74c7ae7cfec624829fd305841d4a2482ca8
Vicrorege <anteytim@gmail.com>
2026-08-04 10:27:28 +0300

    fix docerfile

--- src/hooks/useTerminalSocket.ts
+++ src/hooks/useTerminalSocket.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { TOKEN_STORAGE_KEY, WS_BASE_URL } from '@/config/env'
 import { useWebSocket } from './useWebSocket'
 
